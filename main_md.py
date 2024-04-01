59a38820ad358e5dc9c0d1fc520702c60e1caf40
Hongkee Yoon <bluehope@gmail.com>
2024-04-02 00:01:52 +0900

    Refactor model training and saving

--- main_md.py
+++ main_md.py
@@ -9,15 +9,22 @@ import wandb
 device = "cuda" if torch.cuda.is_available() else "cpu"
 wandb.init(project='diffusionMD')
 
+# options for the model and training
+dataset_path = "dataset"
+result_path = "results"
+
+save_interval = 10 # how often to save the model
+num_epochs = 500000
+learning_rate = 1e-3 # 0.00001
 
 #data load
 #(50000, 64, 9)
-x_tensor=np.load("input.npy") 
+x_tensor=np.load(os.path.join(dataset_path, "input.npy"))
 d1,d2,d3 = np.shape(x_tensor)
 x_tensor_norm, mean_list_x, std_list_x = normalize_md(x_tensor)
 x_tensor_norm=np.reshape(x_tensor_norm, [d1,1,d2,d3])
 x_tensor_norm=torch.from_numpy(x_tensor_norm)
-y_tensor=np.load("output.npy")
+y_tensor=np.load(os.path.join(dataset_path, "output.npy"))
 d1,d2,d3 = np.shape(y_tensor)
 y_tensor_norm, mean_list_y, std_list_y = normalize_md(y_tensor)
 y_tensor_norm=np.reshape(y_tensor_norm, [d1,1,d2,d3]) 
@@ -46,14 +53,19 @@ print("Num params: ", sum(p.numel() for p in model.parameters()))
 print(model)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
-optimizer = Adam(model.parameters(), lr=0.00001)
-num_epochs = 5
+optimizer = Adam(model.parameters(), lr=learning_rate)
 
+
+iter_cnt_total = 0
 for epoch in range(0, num_epochs):  # loop over the dataset multiple times
     running_loss = 0.0
+    mean_loss = 0.0
+    iter_cnt_per_epoch = 0
     for k in ["file_1"]:
         print('File index',k)
         trainN=5000
+        
+        
         for step in range(0,trainN-batch_size,batch_size):
             # get the inputs; data is a list of [inputs, labels]
             indices = np.random.permutation(np.arange(start=step, stop=step+batch_size))
@@ -64,10 +76,18 @@ for epoch in range(0, num_epochs):  # loop over the dataset multiple times
             loss = get_loss_cond(model, input_batch.float().cuda(), t, label_batch.float().cuda())
             loss.backward()
             optimizer.step()
-            wandb.log({'epoch': epoch, 'loss': loss})
+            wandb.log({'loss': loss}, step=iter_cnt_total)
             print('Epoch',epoch, 'Step',step, 'Loss',loss)
-    torch.save(model.state_dict(), './Diffusion_MD_trial'+str(num_epochs)+'.pt')
-    print('Model saved')
+            mean_loss += loss.item()
+            iter_cnt_per_epoch += 1
+            iter_cnt_total += 1
+        
+    mean_loss /= iter_cnt_per_epoch
+    print('Epoch',epoch, 'Mean Loss',mean_loss)
+    wandb.log({'mean_loss': mean_loss}, step=iter_cnt_total)
+    if epoch % save_interval == 0:
+        torch.save(model.state_dict(), os.path.join(result_path, './Diffusion_MD_trial'+str(num_epochs)+'.pt'))
+        print('Model saved')
 
 #Inference
 Nens = 20
@@ -93,5 +113,5 @@ for k in range(0, Nsteps):
 print(pred.shape, te_y.shape) #(100, 20, 1, 64, 9) torch.Size([100, 1, 64, 9])
 pred_denorm = denormalize_md_pred(pred, mean_list_y, std_list_y)
 te_y_denorm = denormalize_md(te_y, mean_list_y, std_list_y)
-np.savez('./predicted_diffusion_md_'+str(lamda_reg),pred,te_y_denorm[:Nsteps])
+np.savez(os.path.join(result_path, './predicted_diffusion_md_'+str(lamda_reg)),pred,te_y_denorm[:Nsteps])
 print('Saved Predictions')
