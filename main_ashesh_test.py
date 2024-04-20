75daf277557fb6067784ab6fec3e78a99a44a2fb
Soo Kim <90995131+fastscience-ai@users.noreply.github.com>
2024-04-20 09:28:06 +0900

    Add files via upload

--- main_ashesh_test.py
+++ main_ashesh_test.py
@@ -1,7 +1,7 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 from functions import *
-from transformer import *
+from transformer_ashesh import *
 from data_loader_one_step_UVS import load_test_data
 from data_loader_one_step_UVS import load_train_data
 import wandb
@@ -9,7 +9,7 @@ import wandb
 device = "cuda" if torch.cuda.is_available() else "cpu"
 x = torch.rand([100,3,128,128]).float().cuda()
 model = Transformer(num_atoms=128, input_size=128, dim=128,
-                 depth=3, heads=4, mlp_dim=512, k=64, in_channels=1)
+                 depth=3, heads=4, mlp_dim=512, k=64, in_channels=3)
 print("Num params: ", sum(p.numel() for p in model.parameters()))
 print(model)
 device = "cuda" if torch.cuda.is_available() else "cpu"
