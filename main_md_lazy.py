fixed normalization, samplers

main_md_lazy.py
@@ -15,13 +15,13 @@ from torch.utils.data import DataLoader, TensorDataset
 #%%
 
 parser = get_parser()
-args = parser.parse_args([])
+args = parser.parse_args()
 print(args.root, args.temperature, args.temperature[0])
 
 # dataset selection
 dset_generator = None
-temp_ = args.temperature
-use_temp_embed = len(temp_) >= 1
+tmep_ = args.temperature
+use_temp_embed = len(tmep_) >= 1
 # if len(temp_list) == 0:
 #     dset_generator = argon_dataset
 # elif len(temp_list) >= 1:
@@ -34,14 +34,11 @@ dset_generator = argon_dataset_rev
 loss_calculator = None
 next_frame_sampler = None
 if args.how_to_sample == "one_step":
-    loss_calculator = get_loss_cond
+    loss_calculator = get_loss_cond_orig
     next_frame_sampler = sample_one_step
-elif args.how_to_sample == "next_frame":
-    loss_calculator = get_loss_cond_rev
-    next_frame_sampler = sample_next_frame
-elif args.how_to_sample == "direct":
-    loss_calculator = get_loss_cond_direct
-    next_frame_sampler = None
+elif args.how_to_sample == "one_step_diff":
+    loss_calculator = get_loss_cond_diff
+    next_frame_sampler = sample_one_step_diff
 else:
     raise ValueError("Invalid choice of sampling method! Choose one_step or next_frame")
 
@@ -49,7 +46,6 @@ tr_x, te_x, tr_y, te_y, \
 mean_list_x, std_list_x, mean_list_y, std_list_y, \
 TRAIN_SIZE, TEST_SIZE, tr_ts, te_ts \
      = dset_generator(args)
-
 #%%
 print(te_x.shape, tr_x.shape)
 print(tr_ts.shape)
@@ -77,13 +73,13 @@ if num_epochs > 10:
 
 if not args.exp_name:
     exp_name = f"md_ep{num_epochs}_one_step_noise_\
-single_temp_{args.temperature}K_T{args.timesteps}_lr{learning_rate}_orig_{args.how_to_sample}"
+single_temp_{args.temperature}K_T{args.timesteps}_lr{learning_rate}_lazy_{args.how_to_sample}"
 else:
     exp_name = args.exp_name
 print("exp name : ", exp_name)
 
-# wandb.init(project='diffusionMD')
-# wandb.run.name = exp_name
+wandb.init(project='diffusionMD')
+wandb.run.name = exp_name
 
 model_save_path = os.path.join('./', args.result_path, exp_name)
 if not os.path.exists(model_save_path):
@@ -106,51 +102,51 @@ if lr_milestones:
     scheduler = MultiStepLR(optimizer, milestones=lr_milestones, gamma=args.lr_gamma)
 
 #%%
-# iter_cnt_total = 0
-# for epoch in range(0, num_epochs):  # loop over the dataset multiple times
-#     running_loss = 0.0
-#     mean_loss = 0.0
-#     iter_cnt_per_epoch = 0
+iter_cnt_total = 0
+for epoch in range(0, num_epochs):  # loop over the dataset multiple times
+    running_loss = 0.0
+    mean_loss = 0.0
+    iter_cnt_per_epoch = 0
 
-#     trainN=tr_x.shape[0]
+    trainN=tr_x.shape[0]
     
-#     for step in range(0,trainN-batch_size,batch_size):
-#         # get the inputs; data is a list of [inputs, labels]
-#         indices = np.random.permutation(np.arange(start=step, stop=step+batch_size))
-#         input_batch, label_batch = tr_x[indices,:,:,:], tr_y[indices,:,:,:]
-#         input_temp = tr_ts[indices]
+    for step in range(0,trainN-batch_size,batch_size):
+        # get the inputs; data is a list of [inputs, labels]
+        indices = np.random.permutation(np.arange(start=step, stop=step+batch_size))
+        input_batch, label_batch = tr_x[indices,:,:,:], tr_y[indices,:,:,:]
+        input_temp = tr_ts[indices]
 
-#         t = torch.randint(1, T, (1,), device=device).long() 
-#         t = t.repeat(batch_size)
-#         #print(t)
+        t = torch.randint(1, T, (1,), device=device).long() 
+        t = t.repeat(batch_size)
+        #print(t)
 
-#         x_0 = input_batch.float().to(device)
-#         label_batch = label_batch.float().to(device)
+        x_0 = input_batch.float().to(device)
+        label_batch = label_batch.float().to(device)
         
-#         # zero the parameter gradients
-#         optimizer.zero_grad()      
+        # zero the parameter gradients
+        optimizer.zero_grad()      
 
-#         loss = loss_calculator(model, input_batch.float().cuda(), t,
-#                              label_batch.float().cuda(), input_temp.float().cuda())
+        loss = loss_calculator(model, input_batch.float().cuda(), t,
+                             label_batch.float().cuda(), input_temp.float().cuda())
 
-#         loss.backward()
-#         optimizer.step()
-#         wandb.log({'loss': loss}, step=iter_cnt_total)
-#         print('Epoch',epoch, 'Step',step, 'Loss',loss)
-#         mean_loss += loss.item()
-#         iter_cnt_per_epoch += 1
-#         iter_cnt_total += 1
+        loss.backward()
+        optimizer.step()
+        wandb.log({'loss': loss}, step=iter_cnt_total)
+        print('Epoch',epoch, 'Step',step, 'Loss',loss)
+        mean_loss += loss.item()
+        iter_cnt_per_epoch += 1
+        iter_cnt_total += 1
 
-#         # remove gradient info
-#         # del x_0, label_batch, x_noisy, noise, u, loss
-#     if scheduler:
-#         scheduler.step()
-#     mean_loss /= iter_cnt_per_epoch
-#     print('Epoch',epoch, 'Mean Loss',mean_loss)
-#     wandb.log({'mean_loss': mean_loss}, step=iter_cnt_total)
-#     if epoch % save_interval == 0:
-#         torch.save(model.state_dict(), os.path.join(model_save_path, str(epoch+1)+'.pt'))
-#         print('Model saved')
+        # remove gradient info
+        # del x_0, label_batch, x_noisy, noise, u, loss
+    if scheduler:
+        scheduler.step()
+    mean_loss /= iter_cnt_per_epoch
+    print('Epoch',epoch, 'Mean Loss',mean_loss)
+    wandb.log({'mean_loss': mean_loss}, step=iter_cnt_total)
+    if epoch % save_interval == 0:
+        torch.save(model.state_dict(), os.path.join(model_save_path, str(epoch+1)+'.pt'))
+        print('Model saved')
 
 #%%
 
@@ -183,40 +179,36 @@ with torch.no_grad():
         print('time step',k)   
         if (k==0):
             for ens in range (0, Nens):
-                if args.how_to_sample == "one_step":
+                if "one_step" in args.how_to_sample:
                     #tt = torch.randint(0, T, (1,), device=device).long() # diffusion time step--> random tt 
                     tt =  torch.tensor([T-1], device=device).long() # 이제 randn이니까?
                 else:
                     tt =  torch.tensor([T-1], device=device).long()
                 x_0 = te_x[0,:,:,:].reshape([1,1,64,9]).float().to(device)
                 # generate from pure noise : 
-                x_noisy = torch.randn_like(x_0)
+                x_noisy, noise = forward_diffusion_sample(x_0, tt, device)
+                x_noisy = x_noisy.unsqueeze(1)
                 #print(x_noisy.device, x_0.device, tt.device)
                 cond = x_0
                 # sample
-                if next_frame_sampler:
-                    u = next_frame_sampler(model, x_noisy, x_0, tt, temp_cond)
-                else:
-                    u = model(x_noisy, x_0, tt, temp_cond)
+                u = next_frame_sampler(model, x_noisy, x_0, tt, temp_cond)
                 pred[k,ens,:,:,:] = np.squeeze(u.detach().cpu().numpy())
         else:
             mean_traj = torch.from_numpy(np.mean(pred [k-1,:,:,:,:],0).reshape([1,1,64,9])).float().to(device) 
             # choose random ensemble? error 누적될수도
             single_traj = torch.from_numpy(pred [k-1,0,:,:,:].reshape([1,1,64,9])).float().to(device)
             for ens in range (0, Nens):
-                if args.how_to_sample == "one_step":
+                if "one_step" in args.how_to_sample:
                     # tt = torch.randint(0, T, (1,), device=device).long() # diffusion time step--> random tt 
                     tt =  torch.tensor([T-1], device=device).long()
                 else:
                     tt =  torch.tensor([T-1], device=device).long()
                 # generate from pure noise : 
-                x_noisy = torch.randn_like(x_0)
+                x_noisy, noise = forward_diffusion_sample(single_traj, tt, device)
+                x_noisy = x_noisy.unsqueeze(1)
                 cond = single_traj
                 # sample
-                if next_frame_sampler:
-                    u = next_frame_sampler(model, x_noisy, single_traj, tt, temp_cond)
-                else:
-                    u = model(x_noisy, single_traj, tt, temp_cond)
+                u = next_frame_sampler(model, x_noisy, single_traj, tt, temp_cond)
                 pred[k,ens,:,:,:] = np.squeeze(u.detach().cpu().numpy())
 #%%
 print(pred.shape, te_y.shape, np.array(mean_list_y).shape) #(100, 20, 1, 64, 9) torch.Size([100, 1, 64, 9])
@@ -224,7 +216,7 @@ print(pred.shape, te_y.shape, np.array(mean_list_y).shape) #(100, 20, 1, 64, 9) 
 print(pred.shape, te_y.shape) #(100, 20, 1, 64, 9) torch.Size([100, 1, 64, 9])
 pred_denorm = denormalize_md_pred(pred, mean_list_y, std_list_y)
 te_y_denorm = denormalize_md(te_y, mean_list_y, std_list_y)
-np.savez(os.path.join(args.result_path, f'./single_temp/{exp_name}_T'),pred,te_y_denorm[:Nsteps])
+np.savez(os.path.join(args.result_path, f'./single_temp/{exp_name}'),pred,te_y_denorm[:Nsteps])
 print('Saved Predictions')
 
 # %%

multitemp_infer_pipeline.py
@@ -1,15 +1,17 @@
 #%%
 import os
-import wandb
-import argparse
+import torch
+import numpy as np
+
+print(f"CUDA available: {torch.cuda.is_available()}")
+print(f"GPU count: {torch.cuda.device_count()}")
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 from utils.args import get_parser
 from utils.functions import *
-from utils.sampler import *
 from utils.data_loader import *
+from utils.sampler import *
 from models.transformer_multiframe import *
-
 from torch.utils.data import DataLoader, TensorDataset
 
 #%%
@@ -22,17 +24,16 @@ print(args.root, args.temperature, args.temperature[0])
 dset_generator = None
 temp_ = args.temperature
 use_temp_embed = len(temp_) >= 1
-# if len(temp_list) == 0:
-#     dset_generator = argon_dataset
-# elif len(temp_list) >= 1:
-#     dset_generator = argon_dataset_mixed_temp
-# else:
-#     raise ValueError("Invalid temperature range")
+
 dset_generator = argon_dataset_rev
 
 # sampling strategy
 loss_calculator = None
 next_frame_sampler = None
+
+# Manul sampler selection
+args.how_to_sample = "one_step"
+
 if args.how_to_sample == "one_step":
     loss_calculator = get_loss_cond
     next_frame_sampler = sample_one_step
@@ -45,116 +46,26 @@ elif args.how_to_sample == "direct":
 else:
     raise ValueError("Invalid choice of sampling method! Choose one_step or next_frame")
 
+print("sampler", next_frame_sampler," temperature : ", temp_)
+
 tr_x, te_x, tr_y, te_y, \
 mean_list_x, std_list_x, mean_list_y, std_list_y, \
 TRAIN_SIZE, TEST_SIZE, tr_ts, te_ts \
      = dset_generator(args)
 
-#%%
-print(te_x.shape, tr_x.shape)
-print(tr_ts.shape)
-print(TRAIN_SIZE, TEST_SIZE)
-print(mean_list_x)
-print("sampler", next_frame_sampler)
-print("loss_calculator", loss_calculator)
-
-#%%
-
-#torch.cuda.set_device(0)
+# %%
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# options for the model and training
-root = args.root
-dataset_path = args.dataset_path
-batch_size = args.batch_size
-save_interval = args.save_interval # how often to save the model
-num_epochs = args.num_epochs
-learning_rate = args.learning_rate # 0.00001
-
-lr_milestones = None
-if num_epochs > 10:
-    lr_milestones = [num_epochs//3, num_epochs//3*2]
-
-if not args.exp_name:
-    exp_name = f"md_ep{num_epochs}_one_step_noise_\
-single_temp_{args.temperature}K_T{args.timesteps}_lr{learning_rate}_orig_{args.how_to_sample}"
-else:
-    exp_name = args.exp_name
-print("exp name : ", exp_name)
-
-# wandb.init(project='diffusionMD')
-# wandb.run.name = exp_name
-
-model_save_path = os.path.join('./', args.result_path, exp_name)
-if not os.path.exists(model_save_path):
-    os.makedirs(model_save_path)
-
-
-#%%
-
-#model = SimpleUnet()
 model = Transformer(num_atoms=64, input_size=9, dim=64,
                  depth=3, heads=4, mlp_dim=256, k=64, in_channels=1,
                  use_temp_embed=use_temp_embed)
-print("Num params: ", sum(p.numel() for p in model.parameters()))
-print(model)
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
-optimizer = Adam(model.parameters(), lr=learning_rate)
-scheduler = None
-if lr_milestones:
-    scheduler = MultiStepLR(optimizer, milestones=lr_milestones, gamma=args.lr_gamma)
+model = model.to(device)
 
-#%%
-# iter_cnt_total = 0
-# for epoch in range(0, num_epochs):  # loop over the dataset multiple times
-#     running_loss = 0.0
-#     mean_loss = 0.0
-#     iter_cnt_per_epoch = 0
+num_epochs = args.num_epochs
+exp_name = "md_ep2501_one_step_noise_\
+    single_temp_[300, 500, 1000]K_T300_lr0.001"
+num_epochs = 2501
 
-#     trainN=tr_x.shape[0]
-    
-#     for step in range(0,trainN-batch_size,batch_size):
-#         # get the inputs; data is a list of [inputs, labels]
-#         indices = np.random.permutation(np.arange(start=step, stop=step+batch_size))
-#         input_batch, label_batch = tr_x[indices,:,:,:], tr_y[indices,:,:,:]
-#         input_temp = tr_ts[indices]
-
-#         t = torch.randint(1, T, (1,), device=device).long() 
-#         t = t.repeat(batch_size)
-#         #print(t)
-
-#         x_0 = input_batch.float().to(device)
-#         label_batch = label_batch.float().to(device)
-        
-#         # zero the parameter gradients
-#         optimizer.zero_grad()      
-
-#         loss = loss_calculator(model, input_batch.float().cuda(), t,
-#                              label_batch.float().cuda(), input_temp.float().cuda())
-
-#         loss.backward()
-#         optimizer.step()
-#         wandb.log({'loss': loss}, step=iter_cnt_total)
-#         print('Epoch',epoch, 'Step',step, 'Loss',loss)
-#         mean_loss += loss.item()
-#         iter_cnt_per_epoch += 1
-#         iter_cnt_total += 1
-
-#         # remove gradient info
-#         # del x_0, label_batch, x_noisy, noise, u, loss
-#     if scheduler:
-#         scheduler.step()
-#     mean_loss /= iter_cnt_per_epoch
-#     print('Epoch',epoch, 'Mean Loss',mean_loss)
-#     wandb.log({'mean_loss': mean_loss}, step=iter_cnt_total)
-#     if epoch % save_interval == 0:
-#         torch.save(model.state_dict(), os.path.join(model_save_path, str(epoch+1)+'.pt'))
-#         print('Model saved')
-
-#%%
-
-# load weights to the model
 try : 
     model.load_state_dict(torch.load(os.path.join(f'./{args.result_path}/{exp_name}/{num_epochs}'+'.pt')))
     print("model successfully loaded")
@@ -163,7 +74,7 @@ except:
     raise Exception("model not loaded")
 
 
-#Inference
+#%%
 Nens = 1
 Nsteps = 2000 # number of experiments
 t_to_simulate = args.t_to_simulate
@@ -218,14 +129,13 @@ with torch.no_grad():
                 else:
                     u = model(x_noisy, single_traj, tt, temp_cond)
                 pred[k,ens,:,:,:] = np.squeeze(u.detach().cpu().numpy())
+
 #%%
 print(pred.shape, te_y.shape, np.array(mean_list_y).shape) #(100, 20, 1, 64, 9) torch.Size([100, 1, 64, 9])
 #Denormalize
 print(pred.shape, te_y.shape) #(100, 20, 1, 64, 9) torch.Size([100, 1, 64, 9])
 pred_denorm = denormalize_md_pred(pred, mean_list_y, std_list_y)
 te_y_denorm = denormalize_md(te_y, mean_list_y, std_list_y)
-np.savez(os.path.join(args.result_path, f'./single_temp/{exp_name}_T'),pred,te_y_denorm[:Nsteps])
+np.savez(os.path.join(args.result_path, f'./{exp_name}_{args.how_to_sample}_{temp_}K'),pred,te_y_denorm[:Nsteps])
 print('Saved Predictions')
-
 # %%
-

utils/data_loader.py
@@ -8,6 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from .functions import *
+from ase import units
 
 
 
@@ -133,7 +134,7 @@ def argon_dataset_rev(args):
     batch_size = args.batch_size
     temp_ = args.temperature
     n_bunch = args.n_bunch # how much previous frames to use
-    n_offset = args.n_offset # from how much distant frames to predict
+    n_offset = int(args.n_offset - 1) # from how much distant frames to predict
 
     # gather dset per temperature
     assert isinstance(temp_, list), "Temperature should be a list"
@@ -163,12 +164,15 @@ def argon_dataset_rev(args):
         # reshape
         d1,d2,d3 = np.shape(y_tensor)
         
-        x_tensor_norm, mean_x, std_x = normalize_md(x_tensor)
+        if args.do_norm : 
+            x_tensor_norm, mean_x, std_x = normalize_md(x_tensor)
+            y_tensor_norm, mean_y, std_y = normalize_md(y_tensor)
+        else:
+            x_tensor_norm, mean_x, std_x = normalize_md_rev(x_tensor)
+            y_tensor_norm, mean_y, std_y = normalize_md_rev(y_tensor)
         x_tensor_norm=np.reshape(x_tensor_norm, [quotient,n_bunch,d2,d3])
         x_tensor_norm=torch.from_numpy(x_tensor_norm)
 
-    
-        y_tensor_norm, mean_y, std_y = normalize_md(y_tensor)
         y_tensor_norm=np.reshape(y_tensor_norm, [quotient,n_bunch,d2,d3]) 
         y_tensor_norm=torch.from_numpy(y_tensor_norm)
 

utils/functions.py
@@ -14,7 +14,7 @@ from typing import List, Dict, Tuple, Set, Union, Optional, Any, Callable
 from .args import get_parser
 
 parser = get_parser()
-args = parser.parse_args()
+args = parser.parse_args([])
 
 import math
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -86,6 +86,40 @@ def get_loss_cond(model, x_0, t, label_batch, temps=None):  #???
     # mse_loss(noise, noise_pred , wavenum_init, lamda_reg)
 
 
+def get_loss_cond_orig(model, x_0, t, label_batch, temps=None):  #???
+    x_noisy, noise = forward_diffusion_sample(x_0, t, device)
+    x_noisy = x_noisy.unsqueeze(1)
+    noise = noise.unsqueeze(1)
+    # if temps is not None:
+    #     temps = temps.reshape(*temps.shape, 1, 1, 1).expand(*temps.shape, 1, 64, 1)
+    #     x_0 = torch.cat([x_0, temps], dim=-1)
+    noise_pred = model(x_noisy, x_0, t, temps)
+    out = x_noisy-noise_pred
+
+    pos_loss = mse_loss(out[..., :3], label_batch[..., :3], wavenum_init, lamda_reg)
+    vel_loss = mse_loss(out[..., -3:], label_batch[..., -3:], wavenum_init, lamda_reg)
+    force_loss = mse_loss(out[..., 3:-3], label_batch[..., 3:-3], wavenum_init, lamda_reg)
+    
+    return pos_loss + vel_loss * 10 + force_loss
+    
+    #mse_loss((x_noisy-noise_pred), label_batch , wavenum_init, lamda_reg)
+
+
+def get_loss_cond_diff(model, x_0, t, label_batch, temps=None):  #???
+    x_noisy, noise = forward_diffusion_sample(x_0, t, device)
+    x_noisy = x_noisy.unsqueeze(1)
+    noise = noise.unsqueeze(1)
+
+    noise_pred = model(x_noisy, x_0, t, temps)
+    difference = label_batch - x_0
+
+    pos_loss = mse_loss(difference[..., :3], noise_pred[..., :3], wavenum_init, lamda_reg)
+    vel_loss = mse_loss(difference[..., -3:], noise_pred[..., -3:], wavenum_init, lamda_reg)
+    force_loss = mse_loss(difference[..., 3:-3], noise_pred[..., 3:-3], wavenum_init, lamda_reg)
+    
+    return pos_loss + vel_loss * 10 + force_loss
+
+
 def get_loss_cond_rev(model, x_0, t, label_batch, temps=None):  
     # add noise to GT data
     x_noisy, noise = forward_diffusion_sample(label_batch, t, device)
@@ -97,6 +131,14 @@ def get_loss_cond_rev(model, x_0, t, label_batch, temps=None):
     # and make the model to predict the noise wirh [x_K, noised_x_K+1]
     noise_pred = model(x_noisy, x_0, t, temps)
     return mse_loss(noise_pred, noise, wavenum_init, lamda_reg)
+
+
+def get_loss_cond_direct(model, x_0, t, label_batch, temps=None):  
+    x_noisy, noise = forward_diffusion_sample(label_batch, t, device)
+    x_noisy = x_noisy.unsqueeze(1)
+
+    output = model(x_noisy, x_0, t, temps)
+    return mse_loss(output, label_batch, wavenum_init, lamda_reg)
 
 
 def get_loss_cond_pos(model, x_0, t, label_batch, temps=None):  #???
@@ -121,18 +163,6 @@ def get_loss_cond_pos(model, x_0, t, label_batch, temps=None):  #???
 
     return pos_loss + vel_loss * 0.5
 
-
-def get_loss_cond_diff(model, x_0, t, label_batch, temps=None):  #???
-    x_noisy, noise = forward_diffusion_sample(x_0, t, device)
-    x_noisy = x_noisy.unsqueeze(1)
-    noise = noise.unsqueeze(1)
-    if temps is not None:
-        temps = temps.reshape(*temps.shape, 1, 1, 1).expand(*temps.shape, 1, 64, 1)
-        x_0 = torch.cat([x_0, temps], dim=-1)
-    noise_pred = model(x_noisy, x_0, t)
-    difference = label_batch - x_0[..., :9]
-    
-    return mse_loss(difference, noise_pred , wavenum_init, lamda_reg)
 
 
 def RK4_sampler(model, x_noisy, cond, tt):
@@ -476,6 +506,18 @@ def normalize_md(x):
                  x[index_1,index_2,i] = (x[index_1,index_2,i]-mean_list[i])/(std_list[i])
     #print(np.amax(x), np.amin(x), mean_list, std_list)
     return x, mean_list, std_list
+
+
+def normalize_md_rev(x):
+    means = np.mean(x, axis=0, keepdims=True)
+    sigmas = np.std(x, axis=0, keepdims=True)
+
+    # normalize pos
+    x[..., :3] = (x[..., :3] - means[..., :3]) / sigmas[..., :3]
+    means[..., 3:] = 0.
+    sigmas[..., 3:] = 1.
+
+    return x, means, sigmas
 
 
 def denormalize_md(x, mean_list, std_list):

utils/sampler.py
@@ -2,19 +2,19 @@ from .functions import *
 from .args import get_parser
 
 parser = get_parser()
-args = parser.parse_args()
+args = parser.parse_args([])
 
-# Define beta schedule
-T = args.timesteps
-betas = linear_beta_schedule(timesteps=T)
-# Pre-calculate different terms for closed form
-alphas = 1. - betas
-alphas_cumprod = torch.cumprod(alphas, axis=0)
-alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
-sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
-sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
-sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
-posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+# # Define beta schedule
+# T = args.timesteps
+# betas = linear_beta_schedule(timesteps=T)
+# # Pre-calculate different terms for closed form
+# alphas = 1. - betas
+# alphas_cumprod = torch.cumprod(alphas, axis=0)
+# alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+# sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+# sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+# sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - alphas_cumprod)
+# posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
 
 
 def sample_one_step(model, x_noisy, condition, t, temp_cond):
@@ -30,6 +30,22 @@ def sample_one_step(model, x_noisy, condition, t, temp_cond):
         noise_pred = model(x_noisy, condition, t, temp_cond)
 
         return x_noisy - noise_pred
+
+
+def sample_one_step_diff(model, x_noisy, condition, t, temp_cond):
+
+    with torch.no_grad():
+        x_noisy = x_noisy.to(device)
+        condition = condition.to(device)
+        t = t.to(device)
+        temp_cond = temp_cond.to(device)
+
+        # noise_pred = model(x_noisy, x_0, t, temps)
+        # difference = label_batch - x_0
+
+        noise_pred = model(x_noisy, condition, t, temp_cond)
+
+        return condition + noise_pred
         
 
 def sample_next_frame(model, x_noisy, condition, tt, temp_cond):
@@ -42,7 +58,7 @@ def sample_next_frame(model, x_noisy, condition, tt, temp_cond):
     x_prev = x_noisy
     
     
-    for t in range(tt[0].item())[::-1]:
+    for t in range(tt[0].item(), -1, -1):
         t_tensor = torch.tensor([t], device=device).long()
         t_tensor = t_tensor.repeat(n_frames)
         noise_pred = model(x_noisy, condition, t_tensor, temp_cond)